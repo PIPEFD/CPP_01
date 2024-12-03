d01e0adc3af2df55173691f8c0e68e2c545a55af
PIPEFD <gamedav68@gmail.com>
2024-12-03 22:37:07 +0100

    falta test para os demas ejercicios ademas de completar los dos harl para hacer el llamado a meotodos privados

---
+++ ex05/src/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pipe <pipe@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 20:21:39 by pipe              #+#    #+#             */
+/*   Updated: 2024/12/03 20:21:41 by pipe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string.h>
+#include <iostream>
+
+class Harl
+{
+    private:
+        void debug();
+        void info();
+        void warning();
+        void error();
+    
+    public:
+    
+};
+
+#endif
